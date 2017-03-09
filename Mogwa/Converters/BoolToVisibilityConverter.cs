b7a68d04b8bbd7a11bc3ca28dfd217e576dad5c3
Ahn DeockHwan <sdzaq@naver.com>
2017-03-09 11:08:47 +0900

    correct git mistakes

---
+++ Mogwa/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Mogwa.Converters
+{
+    /// <summary>
+    /// Bool 값을 Visibility 값으로 변환시켜주는 컨버터
+    /// true: Visibile
+    /// false: Collapsed
+    /// </summary>
+    class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return (bool)value ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
