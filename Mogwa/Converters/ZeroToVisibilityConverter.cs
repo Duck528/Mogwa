74c2dd4b4caf95ecc48aff692a09f67d8d7e280f
Ahn DeockHwan <sdzaq@naver.com>
2017-03-11 01:32:50 +0900

    add untracked files

---
+++ Mogwa/Converters/ZeroToVisibilityConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Mogwa.Converters
+{
+    class ZeroToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            int n = System.Convert.ToInt32(value);
+            if (n == 0)
+                return Visibility.Collapsed;
+            else
+                return Visibility.Visible;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
